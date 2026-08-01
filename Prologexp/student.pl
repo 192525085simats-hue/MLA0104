44370675c8524990f17edd73cde9a938804a64fd
192525085simats-hue <192525085.simats@saveetha.com>
2026-08-01 11:25:29 +0530

    Add files via upload

---
+++ Prologexp/student.pl
@@ -0,0 +1,16 @@
+student(101,rahul).
+student(102,anita).
+student(103,rohit).
+
+teacher(t01,sharma).
+teacher(t02,gupta).
+
+subject(cs101,ai).
+subject(cs102,dbms).
+
+teaches(sharma,ai).
+teaches(gupta,dbms).
+
+studies(rahul,ai).
+studies(anita,dbms).
+studies(rohit,ai).
