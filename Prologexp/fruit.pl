48a99b8264993b4e92f5eaebff1f83833c6663fe
192525085simats-hue <192525085.simats@saveetha.com>
2026-08-01 11:29:48 +0530

    Add files via upload

---
+++ Prologexp/fruit.pl
@@ -0,0 +1,10 @@
+fruit(apple,red).
+fruit(banana,yellow).
+fruit(orange,orange).
+fruit(grapes,green).
+fruit(mango,yellow).
+fruit(strawberry,red).
+
+
+fruit_color(Fruit,Color) :-
+    fruit(Fruit,Color).
